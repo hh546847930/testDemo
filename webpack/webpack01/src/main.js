3b5ab5f2ec20269c3aa8d672dde72a42e61bfcef
hh546847930 <642621377@qq.com>
2020-06-03 17:58:22 +0800

    这是第一次上传

---
+++ webpack/webpack01/src/main.js
@@ -0,0 +1,12 @@
+// Common.js
+const{add,mul}=require('./math')
+console.log(add(20,30));
+console.log(mul(20,30));
+
+
+// ES6  模块化
+
+import{name,age,height}from "./info";
+console.log(name);
+console.log(age);
+console.log(height);
